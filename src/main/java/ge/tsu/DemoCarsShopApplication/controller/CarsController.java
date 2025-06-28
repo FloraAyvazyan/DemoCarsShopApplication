628648061e92101ae2c75387174826a0565602cc
Flora Ayvazyan <flora.ayvazyan@mail.ru>
2025-06-28 14:25:35 +0400

    done logging and testing tasks

--- src/main/java/ge/tsu/DemoCarsShopApplication/controller/CarsController.java
+++ src/main/java/ge/tsu/DemoCarsShopApplication/controller/CarsController.java
@@ -3,7 +3,9 @@ package ge.tsu.DemoCarsShopApplication.controller;
 import ge.tsu.DemoCarsShopApplication.controller.comment.CommentForm;
 import ge.tsu.DemoCarsShopApplication.entiti.Car;
 import ge.tsu.DemoCarsShopApplication.repository.CarRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,40 +13,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Sort;
-
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class CarsController {
 
-    @Autowired
-    private CarRepository repository;
+    private final CarRepository repository;
 
-    // მთავარი გვერდი - index.html აჩვენებს ყველა მანქანას
     @GetMapping("/")
     public String index(Model model) {
+        log.info("Called index()");
         List<Car> carsList = repository.findAll(Sort.by("createdDate").descending());
+        log.debug("Loaded {} cars", carsList.size());
         model.addAttribute("carsList", carsList);
         return "index";
     }
 
-    // კონკრეტული მანქანის გვერდი მისი ID-ით
     @GetMapping("/car/{id}")
     public String car(@PathVariable Long id, Model model) {
+        log.info("Called car() with id={}", id);
         Optional<Car> car = repository.findById(id);
         if (car.isPresent()) {
+            log.debug("Found car with id={} -> {}", id, car.get().getBrand());
             model.addAttribute("car", car.get());
             model.addAttribute("commentForm", new CommentForm(car.get().getId()));
             return "car/single";
         } else {
-            return "error/404"; // თუ მანქანა არ არსებობს
+            log.warn("Car with id={} not found", id);
+            return "error/404";
         }
     }
 
-    // დახმარებისათვის - ტესტური მეთოდი (სურვილისამებრ)
-    @GetMapping("/stuff")
-    @ResponseBody
-    public Car stuff() {
-        return repository.findByBrand("Audi").orElse(null);
-    }
 
 }
